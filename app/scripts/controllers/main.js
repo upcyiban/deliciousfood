521a90c6f7882386b262c8e309d8a41e8681b30d
zhangfuli <1171067930@qq.com>
2017-02-25 20:38:10 +0800

    no message

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name deliciousfoodApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the deliciousfoodApp
+ */
+angular.module('deliciousfoodApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
