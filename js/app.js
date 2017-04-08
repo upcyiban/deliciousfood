e3f19dc30ce95388780e3fc9098cd8bfc1ec6e60
zhangfuli <1171067930@qq.com>
2017-04-08 20:49:16 +0800

    no message

--- js/app.js
+++ js/app.js
@@ -2,38 +2,50 @@ var myApp = angular.module('myApp',[]);
 myApp.config(function ($httpProvider) {
 	$httpProvider.defaults.withCredentials = true;
 });
-myApp.controller('menu',function  ($scope, $http) {
+myApp.run(function ($rootScope){
+	$rootScope.url = 'http://localhost:8086/';
+	$rootScope.foodid = '';
+});
+myApp.controller('menu',function  ($scope, $http, $rootScope) {
+	$http.get($rootScope.url +'choose/findall').then(function (response){
+		console.log(response.data);
+		$scope.foods = response.data;
+	});
 	$scope.search = '';
 	$scope.order = 'name';
 	$scope.desc = 0;
-	$scope.foods = [{
-		name : '红烧czf',
-		region : '黄岛',
-		kind : '臭',
-		price : 10,
-		restaurant : '张氏',
-		num : '5.0',
-		path : './images/hongshao.jpg',
-		introduce :'难吃'
-	},{
-		name : '红烧czf',
-		region : '黄岛',
-		kind : '臭',
-		price : 11,
-		restaurant : '张氏',
-		num : '5.0',
-		path : './images/hongshao.jpg',
-		introduce :'难吃'
-	},{
-		name : '红烧czf',
-		region : '黄岛',
-		kind : '臭',
-		price : 9,
-		restaurant : '张氏',
-		num : '5.0',
-		path : './images/hongshao.jpg',
-		introduce :'难吃'
+});
+
+myApp.controller('disscuss', function ($scope, $rootScope, $location){
+	console.log($location.url());
+	$scope.order = 'time';
+	$scope.text = "";
+	$scope.push = function(){
+		var date = new Date();
+		var timer = date.getMonth()+1+"月"+date.getDate()+"日 "+date.getHours()+"时"+date.getMinutes()+"分";
+		console.log(timer);
+		var push = {
+			id : '123',
+			img : 'images/face.jpg',
+			text : this.text,
+			time : timer
+		}
+		$scope.sayers.push(push);
+	};
+	// $http.get($rootScope.url + $location.url()).then(function (response){
+	// 	$scope.sayers = response.data;
+	// });
+	$scope.sayers = [{
+		id : '123' ,
+		img : 'images/face.jpg' ,
+		text : "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈",
+		time : "timer"
+	},
+	{
+		id : '123' ,
+		img : 'images/face.jpg' ,
+		text : "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈",
+		time : "timer"
 	}];
-	$scope.hello ='helloword';
 });
 
